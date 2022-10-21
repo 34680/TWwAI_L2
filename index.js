a2e6ea55139f4d6a6f7b8e77e6fca2df53aa4206
MJ <test.dev.mj@gmail.com>
2022-10-21 12:22:52 +0200

    34680 Mateusz Jasiak Zad1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const http = require('http');
+
+const server = http.createServer();
+server.on('connection', (socket => {
+   console.log('new connection');
+}))
+
+server.listen(3000);
